3ee4c586d44fe88d1c361cb297338745aba9d5a4
Sincere Villanueva III <svillanueva.git@gmail.com>
2020-11-11 19:24:31 -0800

    First test completed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(entryString, repititions) {
 
-}
+  let finalString = '';
+
+  for (let i = 0; i < repititions; i++) {
+    finalString += entryString;
+  }
+
+  return finalString;
+
+}   
+
 
 module.exports = repeatString
