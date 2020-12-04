adc098f0d7c201da701aa979a1d9015f4d88f715
Sincere Villanueva III <svillanueva.git@gmail.com>
2020-12-04 03:33:08 -0800

    Completed Test 1

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    let finalSum = 0;
+    for (let i = startInt; i <= endInt ; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 }
 
 module.exports = sumAll
