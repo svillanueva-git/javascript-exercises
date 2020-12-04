b7d85dd86875c9f8ecf1275d5b281118416dc240
Sincere Villanueva III <svillanueva.git@gmail.com>
2020-12-04 01:20:00 -0800

    Completed third test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -2,7 +2,11 @@ const removeFromArray = function(originalArray) {
 
     for (let i = 1; i < arguments.length; i++ ) {
         let indexOfElement = originalArray.indexOf(arguments[i]);
+        if (indexOfElement === -1) {
+            continue;
+        } else {
         originalArray.splice(indexOfElement, 1);
+          }
     }
 
     return originalArray;
