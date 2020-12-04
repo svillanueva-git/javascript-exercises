180f7f52251726f4e4d9ee1d571504dddadcd0de
Sincere Villanueva III <svillanueva.git@gmail.com>
2020-12-04 04:04:20 -0800

    Completed Test 4

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,12 +1,16 @@
 const sumAll = function(int1, int2) {
-
-    let begInt = (int1 < int2) ? int1 : int2;
-    let endInt = (int1 > int2) ? int1 : int2;
     let finalSum = 0;
+    if ((int1 < 0) || (int2 < 0)) {
+        return 'ERROR';
+    } else {
 
-    for (let i = begInt; i <= endInt ; i++) {
-        finalSum += i;
-    }
+        let begInt = (int1 < int2) ? int1 : int2;
+        let endInt = (int1 > int2) ? int1 : int2;
+
+        for (let i = begInt; i <= endInt ; i++) {
+            finalSum += i;
+        }
+      }
     return finalSum;
 }
 
