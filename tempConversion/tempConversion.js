ea98c68544dc4b5c1d238f9fa780e0e0dae97135
Sincere Villanueva III <svillanueva.git@gmail.com>
2020-12-31 04:12:44 -0800

    Completed Tests 1-6

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsiusConvert = (temp - 32) * (5/9);
+  return Number(celsiusConvert.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheitConvert = temp * (9/5) + 32;
+  return Number(fahrenheitConvert.toFixed(1));
 }
 
 module.exports = {
