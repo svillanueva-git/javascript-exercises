1b03be79911070137246a1b79582d474879bc228
Sincere Villanueva III <svillanueva.git@gmail.com>
2020-12-31 00:07:28 -0800

    Completed Test 5

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,9 +1,10 @@
 const sumAll = function(int1, int2) {
     let finalSum = 0;
-    if ((int1 < 0) || (int2 < 0)) {
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return 'ERROR';
+    } else if ((int1 || int2) < 0) {
         return 'ERROR';
     } else {
-
         let begInt = (int1 < int2) ? int1 : int2;
         let endInt = (int1 > int2) ? int1 : int2;
 
